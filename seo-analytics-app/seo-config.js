adbbe400b091978f118a587a73f98d7242969e7b
sstene1 <sstene2000@yahoo.com>
2024-09-06 20:47:26 +0000

    Update

---
+++ seo-analytics-app/seo-config.js
@@ -0,0 +1,16 @@
+export default {
+  title: 'Quantum Store - Advanced Gadgets for the Future',
+  description: 'Explore the latest in quantum and interstellar technology at Quantum Store.',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://https://aux1-7ee17c71-a670-4677-9fda-d2aa534ba726.live.alta3.com/',
+    site_name: 'Quantum Store',
+  },
+  twitter: {
+    handle: '@quantumstore',
+    site: '@quantumstore',
+    cardType: 'summary_large_image',
+  },
+};
+
